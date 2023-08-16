425b7f52a8851c3c5143146ec34757dd1cd74c3a
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-08-16 11:02:04 -0700

    basic FORM completed, with try/catch and adding task to DB

--- src/app/tasks/new/page.js
+++ src/app/tasks/new/page.js
@@ -1,5 +1,7 @@
 "use client"
+import { useRouter } from "next/navigation"
 import { useState } from "react"
+
 
 export default function FormPage() {
 
@@ -9,15 +11,36 @@ export default function FormPage() {
     })
 
 
+    const router = useRouter()
+
+    const createTask = async () => {
+        try {
+            const res = await fetch('/api/tasks', {
+                method: 'POST',
+                body: JSON.stringify(newTask),
+                //body: JSON.stringify({ }), //to test error
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            const data = await res.json()
+
+            if (res.status === 200) {
+                router.push('/')
+                //router.refresh()
+            }
+
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
     const handleSumbit = async (e) => {
         e.preventDefault()
-        console.log(newTask)
-
-        const res = await fetch('/api/tasks', {
-            method: 'POST',
-            body: JSON.stringify(newTask)
-        })
-        console.log(res)
+        //console.log(newTask)
+        await createTask()
     }
 
     //const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -31,6 +54,7 @@ export default function FormPage() {
 
     return (
         <div className="h-[calc(100vh-7rem)] flex justify-center items-cneter">
+            <h1 className="font-bold text-3xl">Create Task</h1>
             <form onSubmit={handleSumbit}>
                 <input
                     type="text"
