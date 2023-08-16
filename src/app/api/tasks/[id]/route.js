0482231a66f16446dfc535046df376c03cee6aa8
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-08-15 19:09:36 -0700

    DELETE() in tasks/[id] and some mongoose connection bugs

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,11 +1,11 @@
+import Task from '@/models/Task'
 import { NextResponse } from "next/server";
 import { connectDB } from "@/utils/mongoose";
-import Task from '@/models/Task'
 
 
 export async function GET(request, { params }) {
+    connectDB()
     try {
-        connectDB()
         //Task.findOne({ _id: params.id })
         const taskFound = await Task.findById(params.id)
 
@@ -23,24 +23,50 @@ export async function GET(request, { params }) {
 
 }
 
-export function POST(request, { params }) {
-    return NextResponse.json({
-        message: `Creating ${params.id} Task`
-    })
-}
+// export function POST(request, { params }) {
+//     return NextResponse.json({
+//         message: `Creating ${params.id} Task`
+//     })
+// }
 
-export function DELETE(request, { params }) {
-    return NextResponse.json({
-        message: `Deleting ${params.id} Task`
-    })
+export async function DELETE(request, { params }) {
+    connectDB()
+    try {
+        const taskDeleted = await Task.findByIdAndDelete(params.id)
+
+        if (!taskDeleted)
+            return NextResponse.json({
+                message: "Task not found",
+            }, {
+                status: 404
+            })
+        return NextResponse.json(taskDeleted)
+    } catch (error) {
+        return NextResponse.json(error.message, { status: 400 })
+    }
 }
 
 export async function PUT(request, { params }) {
+    const body = await request.json()
+    connectDB()
+    const data = await request.json()
+
     try {
-        const data = await request.json()
         console.log(data)
         const taskUpdated = await Task.findByIdAndUpdate(params.id, data,
-            { new: true })
+            { new: true }
+
+        )
+
+        if (!taskUpdated)
+            return NextResponse.json(
+                {
+                    message: "Task not found",
+                },
+                {
+                    status: 404,
+                }
+            );
 
         return NextResponse.json(taskUpdated)
     } catch (error) {
