df30dce1a251451ba054f3e0a931380394d56b0e
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-08-15 19:30:22 -0700

    rendering basic json on the frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,22 @@
-export default function Home() {
+import { connectDB } from '@/utils/mongoose'
+import Task from '@/models/Task'
+
+
+async function loadTasks() {
+  connectDB();
+  const tasks = await Task.find();
+  //console.log(tasks)
+  return tasks
+}
+
+export default async function Home() {
+  const tasks = await loadTasks()
   return (
-    <div>HomePage</div>
+    <div>HomePage
+
+      {JSON.stringify(tasks)}
+
+    </div>
+
   )
 }
