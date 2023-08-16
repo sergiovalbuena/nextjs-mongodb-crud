55ad7d6228b8f634294419a3b976e9ad246c265f
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-08-16 14:30:45 -0700

    some styles added to the basic CRUD fullstack app with NEXT.js, ready to be branched

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <main className='container mx-auto px-5 mt-4'>
           {children}
 
